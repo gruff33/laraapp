e428eaf403402e0bacbdb89a69e910c38fe20044
Gareth Richards <garethrichards@gareths-laptop.hursley.uk.ibm.com>
2024-02-08 18:38:59 +0000

    now with testing route to acronym

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Dashboard;
+use App\Livewire\Acronym;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,4 +25,5 @@ Route::middleware([
     'verified',
 ])->group(function () {
     Route::get('/dashboard', Dashboard::class)->name('dashboard');
+    Route::get('/test', Acronym::class)->name('test');
 });
